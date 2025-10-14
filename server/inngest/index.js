c1d0261d79d1844ee336bf8c1603f22698318b06
Phamchicuong <phamcuong@example.com>
2025-10-15 00:41:16 +0700

    vercel

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -19,5 +19,42 @@ const syncUserCreation = inngest.createFunction(
     }
 )
 
+
+//inngest functions to delete user data from a database
+
+const syncUserDeletion = inngest.createFunction(
+    { id: 'delete-user-with-clerk' },
+    { event: 'clerk/user.deleted' },
+    async ({ event }) => {
+        const {id} = event.data
+        await User.findByIdAndDelete(id)
+    }
+)
+
+//inngest functions to update user data from a database
+
+const syncUserUpdation = inngest.createFunction(
+    { id: 'update-user-with-clerk' },
+    { event: 'clerk/user.update' },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: first_name + ' ' + last_name,
+            image: image_url
+        }
+        await User.findByIdAndUpdate(id, userData)
+    }
+)
+
+
+
+
+
 // Create an empty array where we'll export future Inngest functions
-export const functions = [];
+export const functions = [
+    syncUserCreation,
+    syncUserDeletion,
+    syncUserUpdation
+];
